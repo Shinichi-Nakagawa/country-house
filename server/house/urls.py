ce6812d273df193f50c67f1a95bd9584ef6c986b
shinyorke <spirits.is.my.rader@gmail.com>
2014-12-30 17:45:21 +0900

    とりあえずPOSTしてデータを作る所まで成功

--- server/house/urls.py
+++ server/house/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from house import views
 
 
 urlpatterns = [
     url(r'^records/$', views.record_list),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- server/house/views.py
+++ server/house/views.py
@@ -1,60 +1,24 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import permissions
 from house.models import HouseRecord
 from house.serializers import HouseRecordSerializer
 
-
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-@csrf_exempt
-def record_list(request):
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
+def record_list(request, format=None):
     """
     List all code snippets, or create a new snippet.
     """
     if request.method == 'GET':
         records = HouseRecord.objects.all()
         serializer = HouseRecordSerializer(records, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = HouseRecordSerializer(data=data)
+        serializer = HouseRecordSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
-
-@csrf_exempt
-def record_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        record = HouseRecord.objects.get(pk=pk)
-    except HouseRecord.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = HouseRecordSerializer(record)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = HouseRecordSerializer(record, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        record.delete()
-        return HttpResponse(status=204)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
