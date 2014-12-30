4c21c43ac18e6fc0b3f410b003b4f58dd84609f0
shinyorke <spirits.is.my.rader@gmail.com>
2014-12-30 18:51:58 +0900

    API Rootを追加、不要なAPIをバッサリと排除

--- server/house/urls.py
+++ server/house/urls.py
@@ -1,13 +1,11 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from house import views
 
 
 urlpatterns = [
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^records/$', views.RecordList.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^$', views.api_root),
+    url(r'^records/$', views.RecordList.as_view(), name='record-list'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- server/house/views.py
+++ server/house/views.py
@@ -1,10 +1,20 @@
-from django.contrib.auth.models import User
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from house.models import HouseRecord
-from house.serializers import HouseRecordSerializer, UserSerializer
+from house.serializers import HouseRecordSerializer
 from house.permissions import IsOwnerOrReadOnly
+
+
+@api_view(('GET',))
+@permission_classes((permissions.AllowAny, ))
+def api_root(request, format=None):
+    return Response({
+        'records': reverse('record-list', request=request, format=format),
+    })
 
 
 class RecordList(
@@ -30,12 +40,3 @@ class RecordList(
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
